6df4fc983b7e5120a1071f509fe8a431cb786365
inhun <torihome1@naver.com>
2020-02-22 17:00:41 +0900

    first commit

---
+++ src/components/Signin.vue
@@ -0,0 +1,19 @@
+<template>
+<form>
+  ID : <input type="text" name="id"><br>
+  Password : <input type='text' name='password'><br>
+  <input type='submit'>
+</form>
+
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      msg: 'good'
+    }
+  }
+}
+</script>
---
+++ src/signin.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>Login my website!</h1>
+<section class='message'></section>
+<form action='/signin' method="post">
+  ID : <input type="text" name="id"><br>
+  Password : <input type='text' name='password'><br>
+  <input type='submit'>
+</form>
+<form action="/signuppage">
+    <input type="submit" name="signuppage" value="회원가입">
+</form>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
